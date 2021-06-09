42e2865c053f32c8bf436abb9a5a63bc125b58e8
KinjallDodhiaa <kinjal_dodhia@yahoo.com>
2021-06-09 15:08:55 +0200

    Blogs Backend

--- blogs-backend/lib/validation/postRules.js
+++ blogs-backend/lib/validation/postRules.js
@@ -1,13 +1,11 @@
 const { body } = require("express-validator");
 exports.postValidationRules = [
   body("title")
-    .notEmpty()
     .isLength({ min: 5, max: 200 })
     .withMessage(
       "Title is required. Its length should be minimum 5 characters and maximum 200"
     ),
   body("content")
-    .notEmpty()
     .isLength({ min: 20, max: 5000 })
     .withMessage(
       "Content is required. Its length should be minimum 20 characters and maximum 5000"
